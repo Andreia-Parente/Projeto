e8cec3c7a842d4bca0c37ffbdb8b0f9ffee98ef7
Andreia-Parente <andreiaraujoparente@gmail.com>
2023-04-26 14:09:21 +0100

    Update app.js

--- AC3/projeto/resources/js/app.js
+++ AC3/projeto/resources/js/app.js
@@ -6,6 +6,13 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
+//importacao do progresso da app
+import { InertiaProgress } from '@inertiajs/progress';
+
+
+//inicia a InertiaProgress ao app
+InertiaProgress.init()
+
 //cria a app atraves do inertia
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Paginas/${name}.vue`, import.meta.glob('./Paginas/**/*.vue')),
