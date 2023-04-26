13e789ce38fc7d559e9ec85f4440bd5e2b875920
Andreia-Parente <andreiaraujoparente@gmail.com>
2023-04-26 12:06:26 +0100

    Update web.php

--- AC3/projeto/routes/web.php
+++ AC3/projeto/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+//importacao dos controllers
+use App\Http\Controllers\{
+    //importacao do PaginaInicialController
+    HomeController,
+
+};
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//importacao dos routes dos controllers
+//routes publicos
+Route::get('/', [HomeController::class, 'mostrar_pagina_inicial']);
