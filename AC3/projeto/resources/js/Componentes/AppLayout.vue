6b8c56a7568f4b2be062c581f217aea7d9b843bd
Andreia-Parente <andreiaraujoparente@gmail.com>
2023-04-27 15:06:53 +0100

    Update AppLayout.vue

--- AC3/projeto/resources/js/Componentes/AppLayout.vue
+++ AC3/projeto/resources/js/Componentes/AppLayout.vue
@@ -30,12 +30,12 @@
                                 <Dropdown align="right" width="48">
                                     <template #trigger>
                                         <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                            <!--<img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.image" :alt="$page.props.auth.user.name">
-                                            --></button>
+                                           <!-- <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.image" :alt="$page.props.auth.user.name">
+                                           --></button>
 
                                         
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                               <!--- {{ $page.props.auth.user.name }}-->
+                                             <!--{{ $page.props.auth.user.name }}-->
 
                                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -172,19 +172,13 @@ export default{
         DropdownLink,
         ResponsiveNavLink
     },
-        //define os props(ou seja define o tipo de valores que esta no template)
-        props: {
-        users: Object,
-    },
     //define os valores do formulario e do o valor do showNavigationDropdown
     setup(){
+
         const showingNavigationDropdown = ref(false);
 
-
-        
-
         //retorna o formulario e do showingNavigationDropdown para que assim seja acedido no template
-        return {  showingNavigationDropdown };
+        return {  showingNavigationDropdown};
     },
     //cria os metodos 
     methods: {
