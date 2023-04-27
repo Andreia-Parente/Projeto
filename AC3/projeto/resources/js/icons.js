8e3f445e3c983de75b3aa90a061a9ef6164e2116
Andreia-Parente <andreiaraujoparente@gmail.com>
2023-04-27 11:46:56 +0100

    Create icons.js

---
+++ AC3/projeto/resources/js/icons.js
@@ -0,0 +1,10 @@
+//importacao dos icons que quero importar
+import { faUser, faBars} from '@fortawesome/free-solid-svg-icons';
+
+//cria o array para armezenar todos os icons
+const icons = {
+    faUser, faBars
+}
+
+//exporta os icons
+export default icons;
