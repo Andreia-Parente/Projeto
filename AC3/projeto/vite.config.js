5fdbe1e95a9180aece195ef0cf915705dc8540cd
Andreia-Parente <andreiaraujoparente@gmail.com>
2023-04-27 11:44:20 +0100

    Create vite.config.js

---
+++ AC3/projeto/vite.config.js
@@ -0,0 +1,44 @@
+//importacao do vite ao laravel
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+//importacao do vue vite
+import vue from '@vitejs/plugin-vue';
+//importacao do boostrap
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // The Vue plugin will re-write asset URLs, when referenced
+                    // in Single File Components, to point to the Laravel web
+                    // server. Setting this to `null` allows the Laravel plugin
+                    // to instead re-write asset URLs to point to the Vite
+                    // server instead.
+                    base: null,
+ 
+                    // The Vue plugin will parse absolute URLs and treat them
+                    // as absolute paths to files on disk. Setting this to
+                    // `false` will leave absolute URLs un-touched so they can
+                    // reference assets in the public directory as expected.
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+});
+
+
