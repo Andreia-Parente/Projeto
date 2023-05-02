116ad54ee6cfc766c62f15a68c98e50940f32bab
Andreia-Parente <andreiaraujoparente@gmail.com>
2023-05-02 13:25:23 +0100

    Update Perfil.vue

--- AC3/projeto/resources/js/Paginas/Paginas_Privadas/Perfil.vue
+++ AC3/projeto/resources/js/Paginas/Paginas_Privadas/Perfil.vue
@@ -1,112 +1,61 @@
 <template>
-    <AppLayout title="Profile">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Profile
-            </h2>
-        </template>
+    <!--define o nome da pagina-->
+    <Head title="Perfil"></Head>
+    <!--mostra o menu criado na pagina Menu-->
+    <AppLayout>
+        <!--mostra a mensagem a dizer se o utilizador existe ou não-->
+        <div class="alert alert-danger" role="alert" v-if="$page.props.flash.message">
+            {{ $page.props.flash.message }}
+        </div>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                
-
-                    <form @submit.prevent="submit">
-                            <div class="mb-6">
-                                <label
-                                    for="Name"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                                    >Name</label
-                                >
-                                <input
-                                    id="name"
-                                    v-model="form.name"
-                                    type="text"
-                                    class="mt-1 block w-full"
-                                    autocomplete="name"
-                                />
-                                <div
-                                    v-if="form.errors.name"
-                                    class="text-sm text-red-600"
-                                >
-                                    {{ form.errors.name }}
-                                </div>
-                            </div>
-                            <div class="mb-4">
-
-                                <input type="file" @change='atualizar_avatar' class="w-full
-                                        px-4
-                                        py-2
-                                        mt-2
-                                        border
-                                        rounded-md
-                                        focus:outline-none
-                                        focus:ring-1
-                                        focus:ring-blue-600">
-
-
-                                        <div v-show="!verImagem" style="margin-top:10px">
-                            <img :src="getImagens() + form.urlImage" class="rounded-full h-20 w-20 object-cover" />
-                        </div>
-
-                        
-                        <div v-show="verImagem" style="margin-top:10px">
-                           <img :src="verImagem" class="rounded-full h-20 w-20 object-cover" />
-                        </div>
-
-
-
-
-                                
-                                <div
-                                    v-if="form.errors.image"
-                                    class="text-sm text-red-600"
-                                >
-                                    {{ form.errors.image }}
-                                </div>
-                            </div>
-                            <div class="mb-6">
-                                <label
-                                    for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                                    >email</label
-                                >
-                                <input
-                                    id="email"
-                                    v-model="form.email"
-                                    type="email"
-                                    class="mt-1 block w-full"
-                                    autocomplete="email"
-                                    placeholder=""
-                                >
-
-                                <div
-                                    v-if="form.errors.email"
-                                    class="text-sm text-red-600"
-                                >
-                                    {{ form.errors.email }}
-                                </div>
-                            </div>
-                            <button
-                                type="submit"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                :disabled="form.processing"
-                                :class="{ 'opacity-25': form.processing }"
-                            >
-                                Submit
-                            </button>
-                                    </form>
-            </div>
+        <!--cria o titulo da pagina-->
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Perfil
+        </h2>
+        
+        <!--define o formulario do perfil-->
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <form @submit.prevent="atualizarData">
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nome de Utilizador:</label>
+                    <input type="text" class="form-control" id="name" name="name" v-model="form.name"
+                        placeholder="Introduza o nome" autofocus/>
+                </div>
+                <!--cria o input para o ficheiro do avatar-->
+                <div class="mb-4">
+                    <input type="file" @change='atualizar_avatar' class="w-full px-4 py-2 mt-2 border rounded-md 
+                        focus:outline-none focus:ring-1 focus:ring-blue-600">
+                    <!--quando o ficheiro do avatar não é atualizado -->
+                    <div v-show="!verImagem" style="margin-top:10px">
+                        <img :src="getImagens() + form.urlImage" class="rounded-full h-20 w-20 object-cover" />
+                    </div>
+                    <!--quando o ficheiro do avatar é atualizado-->
+                    <div v-show="verImagem" style="margin-top:10px">
+                        <img :src="verImagem" class="rounded-full h-20 w-20 object-cover" />
+                    </div>
+                </div>
+                <div class="mb-6">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" v-model="form.email"
+                        placeholder="Introduza o email" />
+                </div>
+                <!--cria o botao-->
+                <div class="mb-3">
+                    <button class="btn btn-primary d-grid w-100" type="submit">Atualizar Perfil</button>
+                </div>
+            </form>
         </div>
     </AppLayout>
 </template>
 <script>
 //importa os itens para o template
+import { Head, useForm } from '@inertiajs/vue3';
 import AppLayout from '@/Componentes/AppLayout.vue';
-import { useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 export default{
     //adiciona os componentes ao template
     components: {
+        Head,
         AppLayout
     },
     //define os props(ou seja define o tipo de valores que esta no template)
@@ -130,8 +79,8 @@ export default{
     },
     //cria os metodos 
     methods: {
-        //envia os valores do formulario para o route entrar
-        submit(){
+        //envia os valores atualizados do formulario para o route perfil.atualizar
+        atualizarData(){
             this.form.post(route("perfil.atualizar"), {
                 forceFormData: true,
             });
